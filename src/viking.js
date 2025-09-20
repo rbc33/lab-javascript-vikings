c7828fcbe318b6bae34f72f91d91f4ad0633eeb8
rbc33 <ricardobenthem@gmail.com>
2025-09-20 11:24:28 +0200

    iteration 1

--- src/viking.js
+++ src/viking.js
@@ -4,6 +4,12 @@ class Soldier {
 		this.health = health
 		this.strength = strength
 	}
+	attack() {
+		return this.strength
+	}
+	receiveDamage(damage) {
+		this.health -= damage
+	}
 }
 
 // Viking
